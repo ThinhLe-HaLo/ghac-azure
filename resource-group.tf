0c85d4a335f6faeb9be5a6bed6c9ae94a450f193
Le Thinh <lethinhlk@gmail.com>
2026-07-29 23:41:21 +0700

    edit rg-name in rg.tf

--- resource-group.tf
+++ resource-group.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "uat_ghaction" {
-  name     = var.ghaction_rg_name.name
+  name     = var.ghaction_rg_name
   location = var.ghaction_location
 
   tags = var.ghaction_tags
