5447d9f897d515ef29661ae90df5838511fb431a
Le Thinh <lethinhlk@gmail.com>
2026-08-23 23:22:42 +0700

    fix location

--- virtual_network.tf
+++ virtual_network.tf
@@ -1,13 +1,13 @@
 resource "azurerm_virtual_network" "gh-action-vnet" {
 	name                = var.ghaction_vnet_name
-	location            = var.location
+	location            = var.ghaction_location
 	resource_group_name = azurerm_resource_group.uat_ghaction.name
 	address_space       = var.ghaction_vnet_address_space
 }
 
 resource "azurerm_subnet" "gh-action-subnet" {
 	name                 = var.ghaction_subnet_name
-    location             = var.location 
+    location             = var.ghaction_location
 	resource_group_name  = azurerm_resource_group.gh-action-rg.name
 	virtual_network_name = azurerm_virtual_network.uat_ghaction.name
 	address_prefixes     = var.ghaction_subnet_address_prefixes
