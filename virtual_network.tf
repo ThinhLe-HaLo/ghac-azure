b0e619df980d33fa97b9d2544c48180368d05b11
Le Thinh <lethinhlk@gmail.com>
2026-08-23 23:28:18 +0700

    fix subnet

--- virtual_network.tf
+++ virtual_network.tf
@@ -10,5 +10,5 @@ resource "azurerm_subnet" "gh-action-subnet" {
     location             = var.ghaction_location
 	resource_group_name  = azurerm_resource_group.uat_ghaction.name
 	virtual_network_name = azurerm_virtual_network.gh-action-vnet.name
-	address_prefixes     = var.ghaction_subnet_address_prefixes
+	address_prefixes     = var.ghaction_subnet_address_prefix
 }
